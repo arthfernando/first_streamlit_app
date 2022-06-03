4757ecb8dbea7dac4c6a773a1b45ee3a041eb95d
Arthur Bitencourt <32138783+arthfernando@users.noreply.github.com>
2022-06-03 10:12:54 -0300

    Update streamlit_app.py
    update index to Fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # read file from S3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruits')
 
 # user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies
 streamlit.multiselect("Pick Some Fruits: ", list(my_fruit_list.index))
